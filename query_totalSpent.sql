afefdc586b51008d741b22b1e46cbe00e9e635ac
En-Ning Chiang <98069589+enning0814@users.noreply.github.com>
2022-10-17 13:16:24 -0700

    Add files via upload

--- query_totalSpent.sql
+++ query_totalSpent.sql
@@ -1,13 +1,4 @@
-# Second: Write a query that directly answers a predetermined question from a business stakeholder
-## Which brand has the most spend among users who were created within the past 6 months?
-### sql query
-
-```
-select "name", "category", "topBrand", 
-avg("totalSpent") avg_totalSpent, 
-avg("purchasedItemCount") avg_purchaseItemCount,
-sum("purchasedItemCount") sum_purchaseItemCount,
-sum("totalSpent") sum_totalSpent, 
+select "name", "category", "topBrand", sum("totalSpent"), 
 rank()over(order by sum("totalSpent") desc) as rank
 from users u
 right join receipts r on u.id=r."userId"
@@ -16,4 +7,3 @@ inner join brands b on ri."brandCode"=b."brandCode"
 where "name" is not null
 and "createdDate" between '2020-09-01' and '2021-02-28'
 group by "name", "category", "topBrand"
-```
