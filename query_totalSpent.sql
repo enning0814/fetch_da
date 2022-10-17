07cde8151bbffd05c62b552770646a71f04cff70
En-Ning Chiang <98069589+enning0814@users.noreply.github.com>
2022-10-17 13:17:19 -0700

    Delete query_totalSpent.sql

--- query_totalSpent.sql
+++
@@ -1,9 +0,0 @@
-select "name", "category", "topBrand", sum("totalSpent"), 
-rank()over(order by sum("totalSpent") desc) as rank
-from users u
-right join receipts r on u.id=r."userId"
-right join receipts_item ri on r.id=ri.receipt_id
-inner join brands b on ri."brandCode"=b."brandCode"
-where "name" is not null
-and "createdDate" between '2020-09-01' and '2021-02-28'
-group by "name", "category", "topBrand"
